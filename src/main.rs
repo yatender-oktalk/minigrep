42e81d338dd36d3372774e6fd5898a9591f1da87
yatender-oktalk <yatender.nitk@gmail.com>
2020-08-25 17:58:32 +0530

    handling errors

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
 
-  let config = Config::new(&args);
+  let config = Config::new(&args).unwrap_or_else(|err| {
+    println!("Problem with the arguments {}", err);
+    process::exit(1);
+  });
 }
 
 struct Config {
@@ -13,14 +17,14 @@ struct Config {
 }
 
 impl Config {
-  fn new(args: &[String]) -> Config {
+  fn new(args: &[String]) -> Result<Config, &'static str> {
     if args.len() < 3 {
-      panic!("Not enough arguments");
+      return Err("Not enough arguments");
     }
 
     let query = args[1].clone();
     let filename = args[2].clone();
 
-    Config { query, filename }
+    Ok(Config { query, filename })
   }
 }

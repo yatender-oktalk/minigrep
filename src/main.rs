17de0918d07a4d0de39ac8dd7d53c4aae940eec6
yatender-oktalk <yatender.nitk@gmail.com>
2020-08-25 13:17:59 +0530

    Adding config

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 use std::env;
+use std::fs;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+  let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
-    println!("args {:?}", args);
+  let config = Config::new(&args);
+}
+
+struct Config {
+  query: String,
+  filename: String,
+}
+
+impl Config {
+  fn new(args: &[String]) -> Config {
+    if args.len() < 3 {
+      panic!("Not enough arguments");
+    }
+
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
+  }
 }

c8fd196b6d429f0da2e5f1c3f2e7685d097963f9
yatender-oktalk <yatender.nitk@gmail.com>
2020-08-25 19:08:12 +0530

    making test pass

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,15 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-  vec![]
+  let mut results = Vec::new();
+  for line in contents.lines() {
+    println!("{}", line);
+    if line.contains(query) {
+      results.push(line.trim());
+    }
+  }
+  println!("{:?}", results);
+  results
 }
 
 #[cfg(test)]
@@ -42,6 +50,6 @@ mod tests {
     safe, fast, productive.
     pick three.";
 
-    assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+    assert_eq!(vec!["safe, fast, productive."], search(query, contents));
   }
 }

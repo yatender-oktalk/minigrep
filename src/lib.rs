3e5b118f02377413f61a5182738876d90234f70f
yatender-oktalk <yatender.nitk@gmail.com>
2020-08-25 18:56:31 +0530

    Adding failing test

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ pub struct Config {
 }
 
 impl Config {
-  fn new(args: &[String]) -> Result<Config, &'static str> {
+  pub fn new(args: &[String]) -> Result<Config, &'static str> {
     if args.len() < 3 {
       return Err("Not enough arguments");
     }
@@ -19,9 +19,29 @@ impl Config {
   }
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   let contents = fs::read_to_string(config.filename).expect("Something went wrong with the file");
   println!("With text \n{}", contents);
 
   Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  vec![]
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn one_result() {
+    let query = "duct";
+    let contents = "\
+    Rust:
+    safe, fast, productive.
+    pick three.";
+
+    assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+  }
 }
